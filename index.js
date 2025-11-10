5e01d22070e0347e021f6f29d7566f2357d14d9b
javier-tc <javier.tapia.cvl@gmail.com>
2025-11-10 14:25:55 -0300

    FIX: Faltaba index

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger-output.json" assert { type: "json" };
+import usersRouter from "./src/routes/users.js";
+
+const app = express();
+const PORT = 3000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//rutas
+app.use("/users", usersRouter);
+
+//swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//inicio del servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
